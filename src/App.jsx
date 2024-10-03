462149ecd5e483ccada33ca5c8d1aebdb1aa2f1d
Alice Zou <Alicehappy@users.noreply.github.com>
2024-10-03 08:46:22 -0400

    Request api data from express server

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,35 @@ import axios from "axios";
 
 import "./App.css";
 
-const API_URL = "https://api.themoviedb.org/3/";
+const API_URL = "http://localhost:5000/";
 const API_KEY = "?api_key=470ed2f7152cde3cb2e6dbbf664e9f1c";
 //Science Genre id: 878
 //animation id: 16
 
 function App() {
   const [movies, setMovies] = useState([]);
-  // const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
   const fetchMovies = async () => {
     const { data } = await axios.get(
-      `${API_URL}discover/movie${API_KEY}&with_genres=16`
+      `${API_URL}api/movies/discover/movie${API_KEY}&with_genres=16`
     );
-    console.log("movie data: ", data.results);
-    setMovies(data.results || []);
+    console.log("movie data: ", data);
+    setMovies(data || []);
   };
 
   useEffect(() => {
     const loadMovies = async () => {
       try {
         await fetchMovies();
-        // setLoading(false);
       } catch (error) {
         console.error("There is error fetching movies", error);
         setError("Error fetching data");
-        // setLoading(false);
       }
     };
     loadMovies();
   }, []);
 
-  // if (loading) return <p>Loading movies....</p>;
   if (error) return <p>{error}</p>;
   if (!Array.isArray(movies)) return <p>No movies found</p>;
 
@@ -49,7 +45,7 @@ function App() {
               <h3>{movie.title}</h3>
               <p>{movie.overview}</p>
               <p>{movie.release_date}</p>
-              <p>Genre: Science Fiction</p>
+              <p>Genre: Animation</p>
               <img
               src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
               alt={movie.title}
