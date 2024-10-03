3f730d35362889b14aee670af8ea18846eec5c92
brunasfenandes <bruna.fenandes@outlook.com>
2024-10-03 12:19:56 -0400

    Create HomePage functionality

--- pages/HomePage/HomePage.jsx
+++ pages/HomePage/HomePage.jsx
@@ -1,6 +1,73 @@
+import MovieList from '../../src/components/MovieList/MovieList';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './HomePage.scss';
+import { useNavigate } from 'react-router-dom';
+
+const API_URL = "http://localhost:5000/";
+const API_KEY = "?api_key=470ed2f7152cde3cb2e6dbbf664e9f1c";
+//Science Genre id: 878
+//animation id: 16
+
 
 export default function HomePage () {
+  const [movies, setMovies] = useState([]);
+  const [error, setError] = useState(null);
+  const [showMovies, setShowMovies] = useState(false);
+  const [infoMessage, setInfoMessage] = useState(false);
+  const navigate = useNavigate();
+
+  const handleOnSubmit = (event) => {
+    event.preventDefault();
+
+    const inputSearch = event.target.elements.search.value;
+
+    if (inputSearch === "animations") {
+      setShowMovies(true);
+    } else {
+      setShowMovies(false);
+      setInfoMessage(true);
+    }
+  }
+
+  const fetchMovies = async () => {
+    const { data } = await axios.get(
+      `${API_URL}api/movies/discover/movie${API_KEY}&with_genres=16`
+    );
+    console.log("movie data: ", data);
+    setMovies(data || []);
+  };
+
+  useEffect(() => {
+    const loadMovies = async () => {
+      try {
+        await fetchMovies();
+      } catch (error) {
+        console.error("There is error fetching movies", error);
+        setError("Error fetching data");
+      }
+    };
+    loadMovies();
+  }, []);
+
+  const handleOnClick = () => {
+    navigate("/");
+    setInfoMessage(false);
+  }
+
+  if (infoMessage) {
+    return (
+      <>
+        <h1>This is a Animations Tool Search, please insert a valide genre 😉</h1>
+        <button onClick={handleOnClick}>Go Back</button>
+      </>
+    );
+  }
+
+  if (error) return <p>{error}</p>;
+  if (!Array.isArray(movies)) return <p>No movies found</p>;
+
+
   return (
     <>
       <div className='home'>
@@ -8,10 +75,18 @@ export default function HomePage () {
 
         </div>
 
-        <form className='form'>
+        <form className='form' onSubmit={handleOnSubmit}>
           <label htmlFor='searchInput' className='form__label'></label>
           <input type='text' name='search' className='form__search' id='searchInput' placeholder='Enter a movie genre'/>
+          <button type='submit'>Search</button>
         </form>
+
+        {showMovies ? 
+          (<div className='home__movies'>
+            <h2>Animations Movies</h2>
+            <ul>{movies ? <MovieList movies={movies} /> : <p>Loading...</p>}</ul>
+          </div>
+          ) : null}
       </div>
     </>
   )
--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import MovieList from "./components/MovieList/MovieList";
 
 import "./App.scss";
+import HomePage from "../pages/HomePage/HomePage";
 
-const API_URL = "http://localhost:5000/";
-const API_KEY = "?api_key=470ed2f7152cde3cb2e6dbbf664e9f1c";
-//Science Genre id: 878
-//animation id: 16
 
 function App() {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-
-  const fetchMovies = async () => {
-    const { data } = await axios.get(
-      `${API_URL}api/movies/discover/movie${API_KEY}&with_genres=16`
-    );
-    console.log("movie data: ", data);
-    setMovies(data || []);
-  };
-
-  useEffect(() => {
-    const loadMovies = async () => {
-      try {
-        await fetchMovies();
-      } catch (error) {
-        console.error("There is error fetching movies", error);
-        setError("Error fetching data");
-      }
-    };
-    loadMovies();
-  }, []);
-
-  if (error) return <p>{error}</p>;
-  if (!Array.isArray(movies)) return <p>No movies found</p>;
-
   return (
-    <div>
-      <h2>Animations Movies</h2>
-      <ul>{movies ? <MovieList movies={movies} /> : <p>Loading...</p>}</ul>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
